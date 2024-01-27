ce2b2da96f7fb70940913b4d82f45a9e63c4abc3
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-28 00:02:06 +0100

    another test update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function App() {
       <StyledH1>Test</StyledH1>
       <h2>test h2</h2>
       <h3>test h3</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat alias
+        corrupti quae facere a nisi cupiditate labore nobis, dicta atque
+        suscipit quis esse sed molestiae vel harum tempora aliquam? Tenetur!
+      </p>
     </>
   );
 }
