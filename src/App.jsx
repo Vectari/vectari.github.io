323409c23e3ffc11bc39ee8b94a77b3b5bc59c6f
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-28 20:03:10 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import styled from "styled-components";
-
-const StyledH1 = styled.h1`
-  color: gold;
-`;
+import { Header } from "./components/Header";
+import { Info } from "./components/Info";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
     <>
-      <StyledH1>Test</StyledH1>
-      <h2>test h2</h2>
-      <h3>test h3</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat alias
-        corrupti quae facere a nisi cupiditate labore nobis, dicta atque
-        suscipit quis esse sed molestiae vel harum tempora aliquam? Tenetur!
-      </p>
+      <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Info />
+      <Projects />
+      <Contact />
     </>
   );
 }
