fcdad74b0b18a05f7f931834774ae80c0aa1736c
Mateusz Majer <mateusz.majer996@gmail.com>
2024-01-30 23:43:20 +0100

    add projects svg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,11 +3,10 @@ export function Projects() {
     <>
       <h1>Projects</h1>
       <ul>
-        <li>serduszka dwa</li>
-        <li>gamepad tester</li>
-        <li>Inbudget Intive</li>
-        <li>Jamming</li>
-        <li></li>
+        <img src="/serduszkadwa.svg" alt="serduszkadwa logo" />
+        <img src="/gamepadtester.svg" alt="gamepadtester logo" />
+        <img src="/inbudget.svg" alt="inbudget logo" />
+        <img src="/jamming.svg" alt="jamming logo" />
       </ul>
     </>
   );
