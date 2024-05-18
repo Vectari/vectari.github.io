8c9354bba296d7dbcc1ce6b180429dab375f8247
Vectari <mateusz.majer996@gmail.com>
2024-05-18 11:04:58 +0200

    add accents colors

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -5,6 +5,8 @@ export const Theme = {
     secondary: "#333",
     hover: "#555",
     secondaryTransparent: "#33333396",
+    accent: "#CC5500",
+    accent2: "#0047AB",
   },
   width: {
     mobile: "375px",
